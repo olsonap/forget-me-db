f9111dfec0cc0f68d16f8f08293f70ebf874a2bd
Alex <olsonap@uwm.edu>
2023-01-06 21:50:31 -0600

    Basic population of views

---
+++ views.sql
@@ -0,0 +1,50 @@
+CREATE OR REPLACE VIEW usuario AS
+    SELECT * FROM usuarioTbl;
+
+
+CREATE OR REPLACE VIEW address AS
+    SELECT * FROM addressTbl;
+
+
+CREATE OR REPLACE VIEW addressBook AS
+    SELECT * FROM addressBookTbl;
+
+
+CREATE OR REPLACE VIEW card AS
+    SELECT * FROM cardTbl;
+
+
+CREATE OR REPLACE VIEW wallet AS
+    SELECT * FROM walletTbl;
+
+
+CREATE OR REPLACE VIEW order AS
+    SELECT * FROM orderTbl;
+
+
+CREATE OR REPLACE VIEW transaction AS
+    SELECT * FROM transactionTbl;
+
+
+CREATE OR REPLACE VIEW item AS
+    SELECT * FROM itemTbl;
+
+
+CREATE OR REPLACE VIEW orderItem AS
+    SELECT * FROM orderItemTbl;
+
+
+CREATE OR REPLACE VIEW product AS
+    SELECT * FROM productTbl;
+
+
+CREATE OR REPLACE VIEW supplier AS
+    SELECT * FROM supplierTbl;
+
+
+CREATE OR REPLACE VIEW service AS
+    SELECT * FROM serviceTbl;
+
+
+CREATE OR REPLACE VIEW category AS
+    SELECT * FROM categoryTbl;
